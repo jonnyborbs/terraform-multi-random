73fd44878b0c11a28dcfd9b738c38ed90524a7eb
Jon Schulman <25127543+jonnyborbs@users.noreply.github.com>
2022-06-15 07:52:46 -0700

    Update main.tf

--- agent_test_basic/main.tf
+++ agent_test_basic/main.tf
@@ -2,9 +2,11 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
+      version = "2.3.0"
     }
   }
 }
+
 resource "random_id" "random" {
   keepers = {
     uuid = uuid()
